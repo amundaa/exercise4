9866a70665b062c6ee17e16f7718a93f578e1be7
amundaa <amund.aasen@gmail.com>
2013-02-25 11:25:27 +0100

    funk

---
+++ Release/CMakeFiles/main.dir/DependInfo.cmake
@@ -0,0 +1,14 @@
+# The set of languages for which implicit dependencies are needed:
+SET(CMAKE_DEPENDS_LANGUAGES
+  "C"
+  )
+# The set of files for implicit dependencies of each language:
+SET(CMAKE_DEPENDS_CHECK_C
+  "/home/amundaa/sd/exercise4/main.c" "/home/amundaa/sd/exercise4/Release/CMakeFiles/main.dir/main.c.o"
+  )
+SET(CMAKE_C_COMPILER_ID "GNU")
+
+# Targets to which this target links.
+SET(CMAKE_TARGET_LINKED_INFO_FILES
+  "/home/amundaa/sd/exercise4/Release/CMakeFiles/common.dir/DependInfo.cmake"
+  )
---
+++ test/Debug/CMakeFiles/testomp.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+FILE(REMOVE_RECURSE
+  "CMakeFiles/testomp.dir/test.c.o"
+  "testomp.pdb"
+  "testomp"
+)
+
+# Per-language clean rules from dependency scanning.
+FOREACH(lang C)
+  INCLUDE(CMakeFiles/testomp.dir/cmake_clean_${lang}.cmake OPTIONAL)
+ENDFOREACH(lang)
